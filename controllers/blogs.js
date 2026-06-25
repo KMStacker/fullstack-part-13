e08b15062b7bde19a1e2fc4f33e50cd2766c659d
km <kmannisto@hotmail.com>
2026-06-26 02:09:48 +0300

    -> 13.6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 const { Blog } = require('../models')
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  if (!req.blog) {
+    return res.status(404).end()
+  }
+  next()
+}
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
@@ -15,14 +23,15 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.delete('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    await blog.destroy()
-    res.status(204).end()
-  } else {
-    res.status(404).end()
-  }
+router.delete('/:id', blogFinder, async (req, res) => {
+  await req.blog.destroy()
+  res.status(204).end()
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  req.blog.likes = req.body.likes
+  await req.blog.save()
+  res.json(req.blog)
 })
 
 module.exports = router
