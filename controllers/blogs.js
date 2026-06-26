ac7a3960ded463083319906f7b9a48eb5d64f5b7
km <kmannisto@hotmail.com>
2026-06-26 22:39:07 +0300

    -> 3.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -62,6 +62,9 @@ router.get('/', async (req, res) => {
 router.post('/', tokenExtractor, async (req, res, next) => {
   try {
     const user = await User.findByPk(req.decodedToken.id)
+    if (!user) {
+      return res.status(401).json({ error: 'user not found' })
+    }
     const blog = await Blog.create({ ...req.body, userId: user.id })
     return res.json(blog)
   } catch(error) {
